7708c4a0f9ba1c86e8edb8b8a26e7d663830a51b
RohanJ <100120240+Rohanatskit@users.noreply.github.com>
2022-02-28 19:10:14 +0530

    Update sum.sh

--- Sum/sum.sh
+++ Sum/sum.sh
@@ -1,1 +1,16 @@
+#!/bin/bash 
+#Defining_two values 
+A=5 
+B=10 
 
+Sum=$(($A+$B)) 
+echo "Sum: $Sum"
+
+Difference=$(($A-$B)) 
+echo "Difference: $Difference"
+
+Product=$(($Ax$B)) 
+echo "Product: $Product"
+
+Quotient=$(($A/$B)) 
+echo "Quotient is $Quotient"
