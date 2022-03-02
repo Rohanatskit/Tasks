110ff4318aac09d06b1c2bab47af2a4978e6762c
RohanJ <100120240+Rohanatskit@users.noreply.github.com>
2022-03-02 15:32:39 +0530

    Update other.sh

--- Reverse Order/other.sh
+++ Reverse Order/other.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #By using below command you can print the arguments in reverse order
 #echo ""
-printf "%s\n" "1st: $1" "2nd: $2" "3rd: $3" "4th: $4" "5th: $5" | tac
+#To print all given arguments use below command.
+#printf "%s\n" "$@" | tac
+
 #If you don't want limited number of Arguments to be printed you just put "$@" instead of "$1" "$2".
+printf "%s\n" "$1" "$2" "$3" "$4" "$5" | tac
 #And to skip the label you can just type "$1" instead of "Name: $1"
